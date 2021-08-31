6be09d3e127f7ca9e0962e819c934c83bdab2811
Edu-176 <eduardomatanob@gmail.com>
2021-08-31 15:36:22 -0300

    Projeto Criado

---
+++ TesteProjetoWeb.NET/Models/Usuario.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteProjetoWeb.NET.Models
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public int telefone { get; set; }
+        public string cidade { get; set; }
+        public string UF { get; set; }
+
+    }
+}
